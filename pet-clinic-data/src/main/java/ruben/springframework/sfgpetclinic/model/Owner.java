6fd60655a7de6859f0a2bf5bb51cc4b1b00e9597
rubenquintas <rubenquintasnogueiras@gmail.com>
2020-06-09 15:26:36 +0200

    Maven multi-module created

---
+++ pet-clinic-data/src/main/java/ruben/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package ruben.springframework.sfgpetclinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ pet-clinic-data/src/main/java/ruben/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package ruben.springframework.sfgpetclinic.model;
+
+public class Vet extends Person{
+
+	
+	
+}
